34745b3d01d91d3956086188658b9bddbfba02ae
Concurr3ncia <kevinsd872@gmail.com>
2025-04-11 14:12:10 -0500

    Update dependencies and configuration files
    - Upgraded React and React-DOM to version 19.1.0 in package.json and pnpm-lock.yaml for improved performance and features.
    - Added new development dependencies: @svgr/webpack for SVG handling and vite-plugin-compression for asset compression.
    - Updated vite.config.js to include compression and SVGR plugins for better asset management.
    - Removed tailwind.config.js as it is no longer needed.
    - Deleted unused top.gif file to clean up the project.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import compression from "vite-plugin-compression";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    compression({
+      algorithm: "brotliCompress",
+      ext: ".br",
+      deleteOriginFile: false, // opcional, por si querés borrar los archivos sin comprimir
+    }),
+    svgr(),
+  ],
 });
